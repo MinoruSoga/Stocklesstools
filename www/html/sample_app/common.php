e7a0a12e9e3b729d9b3805f7656257d48991ba1a
MinoruSoga <minoru.2021@icloud.com>
2020-09-07 15:54:31 +0900

    create rakute foulder

--- www/html/sample_app/common.php
+++
@@ -1,8 +0,0 @@
-<?php
-  function connect() {
-    $dsn = "mysql:dbname=stockless_system;host=stocklesssalestools_db_1;charset=utf8;";
-    $user = 'root';
-    $password = 'secret';
-	return new PDO($dsn, $user, $password);
-  }
-?>
